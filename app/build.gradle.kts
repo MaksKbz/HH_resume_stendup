9a899e43cb407e64483758482fa7563f627c9047
Maks <100788318+MaksKbz@users.noreply.github.com>
2026-08-11 11:08:13 +0500

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,4 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
-}
-
-android {
-    namespace = "kz.hh.resumebot"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "kz.hh.resumebot"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
-    }
-
-    // Фиксированный ключ: все сборки подписываются одинаково,
-    // поэтому обновления ставятся поверх без удаления приложения
-    signingConfigs {
-        getByName("debug") {
-            storeFile = file("debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-kotlin {
-    jvmToolchain(17)
-}
-
-dependencies {
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    id("com.android.application") version "8.7.2" apply false
+    kotlin("android") version "2.0.21" apply false
 }
