1fa068a45154efe82d8c3c26644f63c223acb814
Maks <100788318+MaksKbz@users.noreply.github.com>
2026-08-17 17:17:16 +0500

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "kz.hh.resumebot"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.7.1"
     }
 
     // Фиксированный ключ: все сборки подписываются одинаково,
